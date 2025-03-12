f3ec106e98b8385ef224143821e6d52551af2000
JenWillems <jen.willems17@gmail.com>
2025-03-12 09:53:11 +0100

    Basic scss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,28 +4,15 @@
 
 @section('content')
 <body>
-    <div class="portfolio-container">
         <header>
-            <h1 class="title">Your Name</h1>
+            <h1 class="title">Jennifer Willems</h1>
             <h2 class="subtitle">Web Developer & Designer</h2>
-        </header>
-
-        <section class="bio">
+        <div class="bio">
             <h3>About Me</h3>
             <p>A brief introduction about your skills, experience, and passion for web development.</p>
-        </section>
+        </div>
+        </header>
 
-        <section class="projects">
-            <h3>Projects</h3>
-            <div class="project-card">
-                <h4>Project Name</h4>
-                <p>Description of the project.</p>
-            </div>
-            <div class="project-card">
-                <h4>Another Project</h4>
-                <p>Description of the project.</p>
-            </div>
-        </section>
         <section class="character-sheet">
             <h2>Character Sheet</h2>
             <div class="character-grid">
@@ -37,8 +24,8 @@
                 </div>
                 <div class="disciplines">
                     <h3>Disciplines</h3>
-                    <p>Dominate: <span class="dots">●●○○○</span></p>
-                    <p>Obtenebration: <span class="dots">●●●○○</span></p>
+                    <p>Design: <span class="dots">●●○○○</span></p>
+                    <p>Front-end <span class="dots">●●●○○</span></p>
                 </div>
                 <div class="attributes">
                     <h3>Attributes</h3>
@@ -48,5 +35,5 @@
                 </div>
             </div>
         </section>
-    </div>
 @endsection
+
