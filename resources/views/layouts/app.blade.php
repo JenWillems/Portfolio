041c94253175ff77027dcc246de184e970fde6f8
JenWillems <jen.willems17@gmail.com>
2025-03-12 13:41:11 +0100

    banner/ project cards

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.cdnfonts.com/css/new-gothic-style" rel="stylesheet">
+
 
     <!-- Styles -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -16,14 +18,6 @@
     <script src="jquery-3.7.1.min.js"></script>
 </head>
 <body>
-<nav>
-    <ul>
-        <li><a href="{{ route('welcome') }}" class="nav-link">Home</a></li>
-        <li><a href="{{ route('about') }}" class="nav-link">About Me</a></li>
-        <li><a href="{{ route('projects') }}" class="nav-link">Projects</a></li>
-        <li><a href="{{ route('leerdoelen') }}" class="nav-link">Leerdoelen</a></li>
-    </ul>
-</nav>
 
 <div id="content">
     @yield('content')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,18 +5,54 @@
 @section('content')
 <body>
         <header>
-            <h1 class="title">Jennifer Willems</h1>
-            <h2 class="subtitle">Web Developer & Designer</h2>
+            <div class="banner_title">
+                <img src="{{ asset('img/template_img.png') }}" alt="Image">
+                <h1 >Jennifer Willems</h1>
+            </div>
+                <h2 class="subtitle">Web Developer & Designer</h2>
         <div class="bio">
-            <h3>About Me</h3>
-            <p>A brief introduction about your skills, experience, and passion for web development.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi asperiores autem blanditiis commodi, consectetur corporis dignissimos dolor fugit id laboriosam molestias necessitatibus odio perferendis quam qui quidem suscipit ut vero
+            . Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dignissimos ex facilis fugiat in laboriosam nemo praesentium quam reprehenderit sit. Corporis inventore laborum obcaecati officia quaerat ratione repellendus rerum voluptas.</p>
+            <button>Switch style</button>
         </div>
-        </header>
 
-        <section class="character-sheet">
+        </header>
+        <section class="example_project">
+            <div class="grid_projects">
+                    <div class="project-card">
+                        <a href="https://www.happyoutdoorliving.nl/" target="_blank">
+                            <img src="{{ asset('/img/template_img.png') }}" alt="Happy Outdoor Living">
+                        </a>
+                        <h5>Happy Outdoor Living</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum debitis eos maiores minima quibusdam sequi tenetur velit! Ab aliquam cumque dignissimos eligendi ipsa, laborum nemo nostrum odit perspiciatis reiciendis rerum!</p>
+                        <button onclick="window.open('https://www.happyoutdoorliving.nl/', '_blank')">Visit Site</button>
+                    </div>
+                        <div class="project-card">
+                            <a href="https://www.happyoutdoorliving.nl/" target="_blank">
+                                <img src="{{ asset('/img/template_img.png') }}" alt="Happy Outdoor Living">
+                            </a>
+                            <h5>Happy Outdoor Living</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum debitis eos maiores minima quibusdam sequi tenetur velit! Ab aliquam cumque dignissimos eligendi ipsa, laborum nemo nostrum odit perspiciatis reiciendis rerum!</p>
+                            <button onclick="window.open('https://www.happyoutdoorliving.nl/', '_blank')">Visit Site</button>
+                        </div>
+                            <div class="project-card">
+                                <a href="https://www.happyoutdoorliving.nl/" target="_blank">
+                                    <img src="{{ asset('/img/template_img.png') }}" alt="Happy Outdoor Living">
+                                </a>
+                                <h5>Happy Outdoor Living</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum debitis eos maiores minima quibusdam sequi tenetur velit! Ab aliquam cumque dignissimos eligendi ipsa, laborum nemo nostrum odit perspiciatis reiciendis rerum!</p>
+                                <button onclick="window.open('https://www.happyoutdoorliving.nl/', '_blank')">Visit Site</button>
+                            </div>
+                    </div>
+
+
+            </div>
+        </section>
+
+        <section class="character_sheet">
             <h2>Character Sheet</h2>
-            <div class="character-grid">
-                <div class="character-info">
+            <div class="character_grid">
+                <div class="character_info">
                     <p><strong>Name:</strong> Jennifer Willems</p>
                     <p><strong>Clan:</strong> Toreador</p>
                     <p><strong>Concept:</strong> Shadow Artist</p>
